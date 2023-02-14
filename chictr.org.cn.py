5e935c8047ae543f1ea1940d2c8b82c70095e942
薛定谔的猫 <1803664082@qq.com>
2023-02-14 15:07:55 +0800

    第一次提交

---
+++ chictr.org.cn.py
@@ -0,0 +1,85 @@
+from selenium import webdriver
+from selenium.common.exceptions import ElementClickInterceptedException
+from selenium.webdriver.common.by import By
+import ddddocr
+from pySelenium import PySelenium
+
+
+class Run(PySelenium):
+    # ------------------- 验证码处理 -------------------
+    # ----<识别验证码并返回
+    Code_Img_Path = './img/codeImg.png'
+
+    def discern_vfcode(self, Code_Img_Path=Code_Img_Path):
+        ocr = ddddocr.DdddOcr()
+        code_loc = self.driver.find_element(By.XPATH, './/input[@name="txtVerifyCode"]/following-sibling::img')
+        code_loc.screenshot(Code_Img_Path)
+        with open(Code_Img_Path, 'rb') as f:
+            img_bytes = f.read()
+        code = ocr.classification(img_bytes)
+        return code
+
+    # ----<碰到验证码后的处理方式
+    def format_vfcode(self):
+        code_input = (By.XPATH, './/div[@role="dialog"]//input[@name="txtVerifyCode"]')
+        code_submit = (By.XPATH, '//*[@id="btnSaveProRes"]')
+        self.locator(code_input).clear()  # 清空
+        vfcode = self.discern_vfcode()  # 调用截取识别函数
+        self.locator(code_input).send_keys(vfcode)  # 输入验证码
+        self.locator(code_submit).click()  # 点击确定
+
+    # ----<验证码识别错误后的处理方式
+    def error_vfcode(self):
+        pass
+
+    # ------------------- Start解析 -------------------
+    def get_data(self):
+        pass
+
+    # ------------------- Start保存 -------------------
+    def save_data(self):
+        pass
+
+    # ------------------- Start翻页 -------------------
+    def turn_pages(self):
+        # 总页数
+        page_count = int(
+            self.locator((By.XPATH, '//*[@id="pgProj"]/span/label')).text.replace('第1页 共', '').replace('页', ''))
+        # 每页的数量
+        page_number = 10
+
+        next_button = (By.XPATH, './/div[@class="pagearea"]/a[last()-1]')
+        for page in range(0, page_count):
+            print(f"---------------------第{page + 1}页")
+            for data_index in range(2, page_number + 2):
+                print(f"第{data_index - 1}条数据---------------------")
+                this_data_link = (By.XPATH, f'.//table[@class="table_list"]//tr[{data_index}]/td[3]/p/a')
+                try:
+                    # 出现验证码时捕获异常并进行处理
+                    self.locator(this_data_link).click()
+                except ElementClickInterceptedException:
+                    self.format_vfcode()
+                # 进入数据详情
+                self.forward()
+                self.get_data()
+
+                # info = (By.XPATH, '/html/body/div[4]/div[3]/table/tbody/tr[1]/td[2]')
+                # info = self.locator(info).text
+                # print(info)
+
+                # 返回数据列表
+                self.back_()
+            # 点击下一页
+            self.locator(next_button).click()
+
+
+if __name__ == '__main__':
+    url = 'http://www.chictr.org.cn/searchproj.aspx'
+    browser = webdriver.Chrome()
+    browser.implicitly_wait(10)
+    start = Run(browser)
+    start.max_win()
+    start.visit_url(url)
+    start.turn_pages()
+
+    browser.quit()
